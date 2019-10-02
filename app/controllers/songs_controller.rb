f6bb99968bc500a3c8899bc90da0e95f6bc7cb59
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-02 20:48:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,13 +47,16 @@ class SongsController < ApplicationController
   end
 
   def edit
-    song = Song.find_by(id: params[:id])
+
     if params[:artist_id]
       artist = Artist.find_by(id: params[:artist_id])
-
-      if artist.nil? || song.artist != artist
+      @song = Artist.songs.find_by(id: params[:id])
+      #if artist.nil? || song.artist != artist
+      if @song.nil?
         redirect_to artists_path
       end
+    else
+      @song = Song.find_by(id: params[:id])
     end
   end
 
